10a6a0510a4aa8ea5b473bb0b913627c7849e579
Sajjad Ahmad <jogimar14@gmail.com>
2021-03-04 14:53:09 +0500

    install css and file loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
 
 module.exports = {
+  'mode': 'none',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ["style-loader", "css-loader", { loader: 'file-loader',},],
+      },
+    ],
   },
 };
