094d9ead830f4e6913ddef7ebafe23a15ffbef08
Sajjad Ahmad <jogimar14@gmail.com>
2021-03-08 12:09:31 +0500

    convert http to https

--- src/weatherData.js
+++ src/weatherData.js
@@ -1,6 +1,6 @@
 const getWeatherData = async (location) => {
   const key = 'ab1bb349d168e0577aa7f9a8a76025a4';
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`;
   const weatherData = await fetch(url);
   return weatherData;
 };
